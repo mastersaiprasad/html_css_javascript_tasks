861168655feba0a478473a2d7f8b874f079d9222
mastersaiprasad <76834979+mastersaiprasad@users.noreply.github.com>
2021-01-04 23:59:34 +0530

    Add files via upload
    this task is a simple one on element insertion in an  array at last position.

---
+++ arrays.js
@@ -0,0 +1,12 @@
+// Problem statement
+
+/* take an array add the elements to it and then print it...
+after that add an element at the last of array and print it.*/
+
+
+
+
+var arr=[1,3,5,"sai", "prasad"];
+console.log(arr);
+arr.push("hellon");
+console.log(arr);
