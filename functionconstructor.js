a354822363422380c41301643bc5bf3d6fa31532
mastersaiprasad <76834979+mastersaiprasad@users.noreply.github.com>
2021-01-18 20:21:19 +0530

    Add files via upload
    this is an example on how to use function constructor.

---
+++ functionconstructor.js
@@ -0,0 +1,16 @@
+function Laptops(name,brand,price)
+{
+    this.laptopname=name;
+    this.laptopbrand=brand;
+    this.laptopprice=price;
+}
+var laptop1=new Laptops("acer 1.2","acer",50000);
+console.log(laptop1);
+var laptop2=new Laptops("dell inspiron","dell",68000);
+console.log(laptop2);
+var laptop3=new Laptops("lenovo 5.0","Lenovo",55000);
+console.log(laptop3);
+var laptop4=new Laptops("Asus 2.3","ASUS",48000);
+console.log(laptop4);
+var laptop5=new Laptops("hp 1.2","HP",62000);
+console.log(laptop5);
